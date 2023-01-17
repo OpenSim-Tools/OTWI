07215f1605452c046ea2268d6ec67556cd566dc4
OpenSim Tools <122408461+OpenSim-Tools@users.noreply.github.com>
2023-01-17 22:32:59 +0100

    Update setup.php
    ....
    
    Signed-off-by: OpenSim Tools <122408461+OpenSim-Tools@users.noreply.github.com>

--- setup.php
+++ setup.php
@@ -1,7 +1,1 @@
 <?php
-
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
